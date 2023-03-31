90f25892789303ba5898184fb7f4a3bb87dc84a2
yhoogstrate <y.hoogstrate@gmail.com>
2023-03-31 11:19:51 +0200

    updates

---
+++ R/function__check_config.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env R
+
+check_config <- function(seurat_obj) {
+  stopifnot(!is.null(seurat_obj@misc$dataset))
+  stopifnot(!is.null(seurat_obj@misc$sample_name))
+  stopifnot(!is.null(seurat_obj@misc$sample_full_name))
+  stopifnot(!is.null(seurat_obj@misc$tumor_type))
+  
+  stopifnot(seurat_obj@misc$random_seed == 42)
+  
+  
+  
+  stopifnot(!is.null(seurat_obj$celltype_annotated))
+  
+  if(seurat_obj@misc$tumor_type == 'Astrocytoma, IDH-mut') {
+    stopifnot(!is.null(seurat_obj$celltype_annotated_venteicher))
+  }
+}
+
--- R/includes.R
+++ R/includes.R
@@ -6,6 +6,7 @@ source('R/function__annotate_read_fractions.R')
 source('R/function__cache_seurat_object.R')
 source('R/function__celltype_reclassification_PSLDA.R')
 source('R/function__flag_uncertain_classifications.R')
+source('R/function__check_config.R')
 
 source('R/load__celltype_markers.R')
 source('R/load__venteicher__IDHmut__programs.R')
