f3c990c0bd5a09f3ddd661f7b35fd29faed95d1e
yhoogstrate <y.hoogstrate@gmail.com>
2023-04-04 16:17:04 +0200

    sav

--- R/function__check_config.R
+++ R/function__check_config.R
@@ -6,7 +6,16 @@ check_config <- function(seurat_obj) {
   stopifnot(!is.null(seurat_obj@misc$sample_full_name))
   stopifnot(!is.null(seurat_obj@misc$tumor_type))
   
+
   stopifnot(!is.null(seurat_obj@misc$max.percentage.mitochondrial))
+
+  stopifnot(!is.null(seurat_obj@misc$min.nFeature_RNA))
+  stopifnot(!is.null(seurat_obj@misc$max.nFeature_RNA))
+  stopifnot(seurat_obj@misc$max.nFeature_RNA > seurat_obj@misc$min.nFeature_RNA)
+  stopifnot(!is.null(seurat_obj@misc$min.nCount_RNA))
+  stopifnot(!is.null(seurat_obj@misc$max.nCount_RNA))
+  stopifnot(seurat_obj@misc$max.nCount_RNA > seurat_obj@misc$min.nCount_RNA)
+  
   
   stopifnot(seurat_obj@misc$random_seed == 42)
   
