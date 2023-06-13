1755b903a92580a72a307069804550881be5991b
yhoogstrate <y.hoogstrate@gmail.com>
2023-06-13 10:06:00 +0200

    adds cycling markers as asset

--- R/load__celltype_markers.R
+++ R/load__celltype_markers.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env R
 
 celltype_markers <- read.csv("assets/celltype_markers.txt")
+celltype_markers_cell_cycle <- read.csv("assets/celltype_markers_cell_cycle.txt",header=T,sep="\t",check.names=F)
 
 
